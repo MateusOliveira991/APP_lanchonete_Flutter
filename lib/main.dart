fc923ebaef48d407cc6b6e76ac2889cee2b76c4b
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-04-09 18:09:05 -0300

    Feat: Estilização LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:lanchonete/core/app_widget.dart';
 
 void main() {
+  //Comando para cortar barras superiores e inferiores.
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MyApp());
--- lib/pages/Login/login_page.dart
+++ lib/pages/Login/login_page.dart
@@ -3,18 +3,54 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Column(
-      children: [
-        Container(
-          child: Image.asset('assets/images/logo1.png'),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+           
+              
+              Colors.black,
+              Color.fromARGB(255, 232, 217, 12),
+              Colors.black,
+              
+            ],
+            
+          ),
         ),
-      ],
-      
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+            Container(
+              child: Image.asset('assets/images/logo.png'),
+              
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                labelText: 'Usuário',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Entrar'),
+              style: ButtonStyle(
+                
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
-  );
-}
-
+    );
+  }
 }
